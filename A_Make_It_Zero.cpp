50c094e7bfc815252f3b7c7a9269dc02bd7b05d9
Aman <349aman349@gmail.com>
2025-06-28 20:45:40 +0530

    Push CF_Questions folder

--- A_Make_It_Zero.cpp
+++ A_Make_It_Zero.cpp
@@ -1,38 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Speed
+// Speed
 #define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
-//Macros
-#define rep(i,j) for(int i=0;i<j;i++)
-#define rrep(i,j) for(int i=j-1;i>=0;i--)
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
 #define all(x) x.begin(), x.end()
 
-//Typedef
+// Typedef
 typedef long long ll;
 typedef pair<int, int> pi;
 typedef vector<int> vi;
-typedef map<int,int> mii;
+typedef map<int, int> mii;
 
 void answertoProb()
 {
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
-    if(n&1){
-        cout<<"4"<<endl;
-        cout<<"1"<<" "<<n-1<<endl;
-        cout<<"1"<<" "<<n-1<<endl;
-        cout<<n-1<<" "<<n<<endl;
-        cout<<n-1<<" "<<n<<endl;
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+    if (n & 1)
+    {
+        cout << "4" << endl;
+        cout << "1" << " " << n - 1 << endl;
+        cout << "1" << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
     }
-    else{
-        cout<<"2"<<endl;
-        cout<<"1"<<" "<<n<<endl;
-        cout<<"1"<<" "<<n<<endl;
+    else
+    {
+        cout << "2" << endl;
+        cout << "1" << " " << n << endl;
+        cout << "1" << " " << n << endl;
     }
+    //  cout<<77<<endl;
+    //  cout<<77<<endl;
+
+    //  cout<<77<<endl; //  cout<<77<<endl;
+
+    //  cout<<77<<endl;
+    //  cout<<77<<endl;
+    //  cout<<77<<endl;
 }
 
 int32_t main()
