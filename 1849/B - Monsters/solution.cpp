7b2ca3b65ecedc2f689d06f7e75d523e448acb8c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:04:44 +0530

    Add Monsters [B] from Codeforces

---
+++ 1849/B - Monsters/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (auto &x : a) {
+      cin >> x;
+      x %= k;
+      if (!x) x = k;
+    }
+    vector<int> ord(n);
+    iota(ord.begin(), ord.end(), 0);
+    stable_sort(ord.begin(), ord.end(), [&](int i, int j) {
+      return a[i] > a[j];
+    });
+    for (auto &x : ord) cout << x + 1 << ' ';
+    cout << endl;
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
