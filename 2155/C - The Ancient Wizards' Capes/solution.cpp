569a5b966b16accbd7c6f41be4d2d90ede723eb8
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:25:22 +0530

    Add The Ancient Wizards' Capes [C] from Codeforces

---
+++ 2155/C - The Ancient Wizards' Capes/solution.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool validar(const vector<int>& x, int n, const vector<int>& a) {
+    bool flag = false;
+    int visibles = 1;
+ 
+    for (int i = 2; i <= n; i++) {
+        if (x[i] == 1)
+            visibles++;
+    }
+ 
+    if (visibles == a[1]) {
+        flag = true;
+ 
+        for (int i = 1; i < n; i++) {
+            if (x[i] == 1 && x[i + 1] == 1)
+                visibles--;
+            else if (x[i] == 0 && x[i + 1] == 0)
+                visibles++;
+ 
+            if (a[i + 1] != visibles) {
+                flag = false;
+                break;
+            }
+        }
+    }
+ 
+    return flag;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> arr(n + 1);
+        for (int i = 1; i <= n; i++) {
+            cin >> arr[i];
+        }
+ 
+        bool flag = true;
+        int cont = 0;
+ 
+        vector<int> sol1(2, 0); // index 0 and 1 are 0
+        vector<int> sol2(2, 0);
+        sol2[1] = 1;
+ 
+        for (int j = 1; j < n; j++) {
+            if (arr[j + 1] - arr[j] > 1) {
+                flag = false;
+                break;
+            }
+ 
+            if (arr[j + 1] - arr[j] == 0) {
+                sol1.push_back(1 - sol1[j]);
+                sol2.push_back(1 - sol2[j]);
+            } else {
+                sol1.push_back(sol1[j]);
+                sol2.push_back(sol2[j]);
+            }
+        }
+ 
+        if (!flag) {
+            cout << 0 << "
+";
+            continue;
+        }
+ 
+        if (validar(sol1, n, arr))
+            cont++;
+ 
+        if (validar(sol2, n, arr))
+            cont++;
+ 
+        cout << cont << "
+";
+    }
+ 
+    return 0;
+}
