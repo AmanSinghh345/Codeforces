7b47c0a54e7188212f9cd339406f15a9d640d0da
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:36:34 +0530

    Add Shuffle [B] from Codeforces

---
+++ 1366/B - Shuffle/solution.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    long long n,x,m;
+    cin>>n>>x>>m;
+   
+    long long l=x,r=x;
+    while(m--){
+        long long  l1,r1;
+        cin>>l1>>r1;
+        if((l1<=l && l<=r1) || (l<=l1 && l1<=r)){
+            l=min(l1,l);
+            r=max(r1,r);
+        }
+    }
+    cout<<r-l+1<<'
+';
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
