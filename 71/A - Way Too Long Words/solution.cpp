44d1aff7bfd73cf82451cadc24f0fe4c5306323c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:05:39 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{
+    string s;
+    cin>>s;
+    string ans="";
+    int between=s.size()-2;
+    if(s.length()<=10) {
+        cout<<s<<"
+";
+        return;
+    }
+    else {
+        ans+=s[0];
+        ans+=to_string(between);
+        ans+=s[s.size()-1];
+        cout<<ans<<"
+";
+        return;
+    }
+    return;
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
