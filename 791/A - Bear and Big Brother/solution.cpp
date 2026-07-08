86e327131480eac451e5153b8cf07442b17b5f6c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:06:07 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{
+    int a,b;
+    cin>>a>>b;
+    int cnt=0;
+    while(a<=b){
+        a*=3;
+        b*=2;
+        cnt++;
+    }
+    cout<<cnt<<endl;
+    return;
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t=1;
+    //cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
