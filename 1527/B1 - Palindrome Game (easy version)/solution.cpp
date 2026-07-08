44e12214191be4585ab9799dd7843aac407b9d86
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:35:02 +0530

    Add Palindrome Game (easy version) [B1] from Codeforces

---
+++ 1527/B1 - Palindrome Game (easy version)/solution.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    bool f=true;
+    int z=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='0') z++;
+    }
+   
+    if(z%2==0 || z==1) {
+        cout<<"BOB"<<"
+";
+        return;
+    }
+    
+    cout<<"ALICE"<<"
+";
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
