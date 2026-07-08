87c0d9e62764fa08ad72e398a659dc0d526b2556
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:35:58 +0530

    Add Divide and Equalize [D] from Codeforces

---
+++ 1881/D - Divide and Equalize/solution.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    map<int,int> mp;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+ 
+        for(int p=2;p*p<=x;p++){
+            while(x%p==0){
+                mp[p]++;
+                x/=p;
+            }
+        }
+        if(x>1){
+            mp[x]++;
+        }
+    }
+ 
+    for(auto it : mp){
+        if(it.second%n!=0){
+            cout<<"NO"<<"
+";
+            return ;
+        }
+    }
+    cout<<"YES"<<"
+";
+    return ;
+ 
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
