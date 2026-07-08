44030002266e1b1cdeb91e42cfad1953e622a770
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:01:46 +0530

    Add Serval and Mocha's Array [A] from Codeforces

---
+++ 1789/A - Serval and Mocha's Array/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{  //Addition of number in the set cannot increase the gcd
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    bool flag=false;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(__gcd(a[i],a[j])<=2) {
+                flag =true;
+                break;
+            }
+        }
+    }
+    if(flag) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+ 
+ 
+    
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
