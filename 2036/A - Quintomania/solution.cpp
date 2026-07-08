ad28dbea6841df162adc160720409d19c4c406ce
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:04:59 +0530

    Add Quintomania [A] from Codeforces

---
+++ 2036/A - Quintomania/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n-1;i++){
+        if(abs(a[i]-a[i+1])!=5 && abs(a[i]-a[i+1])!=7)
+        {cout<<"NO"<<"
+";
+        return;
+        }
+    }
+    cout<<"YES"<<"
+";
+    return;
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
