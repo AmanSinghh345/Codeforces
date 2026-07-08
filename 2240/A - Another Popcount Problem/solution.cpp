633461b11e577e1ef605e8484763fa7071e39079
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:35:54 +0530

    Add Another Popcount Problem [A] from Codeforces

---
+++ 2240/A - Another Popcount Problem/solution.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+#  define popcount __builtin_popcount
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    int cnt=0;
+    for(int bit=0;(1<<bit)<=n;bit++){
+        int copies=min(k,n/(1<<bit));
+        cnt+=copies;
+        n-=copies*(1<<bit);
+    }
+    cout<<cnt<<"
+";
+    return ;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
