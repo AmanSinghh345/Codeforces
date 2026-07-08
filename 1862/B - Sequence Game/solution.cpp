685d95529b09e19f4210c54af6b004335c40b61c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:00:13 +0530

    Add Sequence Game [B] from Codeforces

---
+++ 1862/B - Sequence Game/solution.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+ 
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+ 
+void answertoProb()
+{
+    int n;
+    cin>>n;
+    vi arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+ 
+    }
+    vi ans;
+    ans.push_back(arr[0]);
+    for(int i=1;i<n;i++){
+        if(arr[i]>=arr[i-1])
+          ans.push_back(arr[i]);
+        else{
+            ans.push_back(arr[i]);
+            ans.push_back(arr[i]);
+ 
+        }
+    }
+    cout<<ans.size()<<endl;
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
+ 
+int32_t main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        answertoProb();
+    }
+    return 0;
+}
