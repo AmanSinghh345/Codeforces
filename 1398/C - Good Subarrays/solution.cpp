06842501aebca9d0158053f3e8f663ed6f3063e0
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-08 12:36:25 +0530

    Add Good Subarrays [C] from Codeforces

---
+++ 1398/C - Good Subarrays/solution.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<int> a;
+    for(char c: s){
+        int x=c-'0';
+        a.push_back(x);
+    }
+ 
+    long long cnt=0;
+    long long  pref=0;
+    map<long long ,long long > umap;
+    umap[0]=1;
+    for(int i=0;i<n;i++){
+         pref+=a[i];
+         if(umap.count(pref-(i+1))) cnt+=(umap[pref-(i+1)]);
+         umap[pref-(i+1)]++;
+    }
+    cout<<cnt<<"
+";
+    return ;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
