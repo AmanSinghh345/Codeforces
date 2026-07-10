2c0ac9c9362cab4cbb06064a8bbee71e7c1ac5c1
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-10 15:35:31 +0530

    Add Maximal AND [H] from Codeforces

---
+++ 1669/H - Maximal AND/solution.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    vector<int> bits(31,0);
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        for(int j=0;j<=30;j++){
+            if((x>>j)&1) bits[j]++;
+        }
+    }
+    int ans=0;
+    for(int j=30;j>=0;j--){
+        int need=n-bits[j];
+        if(need<=k){
+            ans|=(1<<j);
+            k-=need;
+        }
+    }
+    cout<<ans<<"
+";
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
