111b9b53031a866f7cfa6685ce9df4b05ad285f8
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-12 13:43:23 +0530

    Add Most socially-distanced subsequence [B] from Codeforces

--- 1364/B - Most socially-distanced subsequence/solution.cpp
+++ 1364/B - Most socially-distanced subsequence/solution.cpp
@@ -38,41 +38,13 @@ void solve() {
     vector<int> a(n);
     for(int i=0;i<n;i++) cin>>a[i];
  
-    if(n==1){
-        cout<<1<<"
-";
-        cout<<a[0]<<"
-";
-        return;
-    }
-    // long long maximum=0;
-    // for(int i=1;i<n;i++){
-    //     maximum+=(a[i]-a[i-1]);
-    // }
+   
     vector<int> ans;
-    ll val=0;
-    bool inc;
-    if(a[0]>a[1]) inc=false;
-    else inc=true;
     ans.push_back(a[0]);
- 
-    for(int i=2;i<n;i++){
-        if(inc){
-            if(a[i-1]<a[i]) {
-                continue;
-            }
-            else{
-                ans.push_back(a[i-1]);
-                inc=false;
-            }
-        }
-        else{
-            if(a[i-1]>a[i]) continue;
-            else {
-                ans.push_back(a[i-1]);
-                inc=true;
-            }
-        }
+    for(int i=1;i<n-1;i++){
+        bool localMaxima=a[i]>a[i-1] && a[i]>a[i+1];
+        bool localMinima=a[i]<a[i-1] && a[i]<a[i+1];
+        if(localMaxima || localMinima) ans.push_back(a[i]);
     }
     ans.push_back(a[n-1]);
     cout<<(int)ans.size()<<"
