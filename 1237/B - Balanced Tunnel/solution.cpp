6692717853339e13ee9468fa57a6d1134350ae3f
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-12 15:00:23 +0530

    Add Balanced Tunnel [B] from Codeforces

---
+++ 1237/B - Balanced Tunnel/solution.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    cin>>n;
+ 
+    map<int,int> exit;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a[i]=x;
+ 
+    }
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        exit[x]=i+1;
+    }
+  
+    int maxi=0;
+    int cnt=0;
+    for(int x : a){
+        int diff=exit[x];
+        if(diff<maxi) cnt++;
+        maxi=max(diff,maxi);
+    }
+    cout<<cnt<<endl;
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
