a7b8089f8fd0a55ad05b040551d560cc4dd86014
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-16 13:30:02 +0530

    Add Iskander and Drawings [A] from Codeforces

---
+++ 2244/A - Iskander and Drawings/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int maxLen=0;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='#') cnt++;
+        else cnt=0;
+        maxLen=max(maxLen,cnt);
+    }
+    cout<<ceil((maxLen+1)/2)<<endl;
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
