8724c2e56fec129b9ca03305a09db1c8877c04b2
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-19 12:43:15 +0530

    Add 2^Sort [G] from Codeforces

---
+++ 1692/G - 2^Sort/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a);
+        a = (a * a);
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    vector<long long> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    int cnt=0;
+    int cons=0;
+    for(int i=0;i<n-1;i++){
+        if(a[i]<2LL*a[i+1]) cons++;
+        else cons=0;
+        if(cons>=k) cnt++;
+    }
+    cout<<cnt<<endl;
+    return;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
