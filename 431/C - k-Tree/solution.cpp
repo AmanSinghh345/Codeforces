7299909efb542d24db0ab633669e51d6ed1c3fa6
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-01 14:06:32 +0530

    Add k-Tree [C] from Codeforces

---
+++ 431/C - k-Tree/solution.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+long long find(int target,int k,int d,int usedBig,vector<vector<long long >>& dp){
+    if(target<0) return 0;
+    if(target==0) return usedBig;
+    if(dp[target][usedBig]!=-1) return dp[target][usedBig];
+    ll take=0;
+    for(int i=1;i<=k;i++){
+        int newUsedBig=usedBig || i>=d;
+        take=(take+find(target-i,k,d,newUsedBig,dp))%MOD;
+    }
+    return dp[target][usedBig]=take;
+}
+void solve() {
+    int n,k,d;
+    cin>>n>>k>>d;
+    vector<vector<long long >> dp(n+1,vector<long long>(2,-1));
+    int usedBig=0;
+    find(n,k,d,usedBig,dp);
+    cout<<dp[n][usedBig];
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
