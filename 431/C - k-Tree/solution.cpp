d95ebfdeaa585f223685cbff3f658e116ec07c38
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-01 14:20:56 +0530

    Add k-Tree [C] from Codeforces

--- 431/C - k-Tree/solution.cpp
+++ 431/C - k-Tree/solution.cpp
@@ -45,10 +45,20 @@ long long find(int target,int k,int d,int usedBig,vector<vector<long long >>& dp
 void solve() {
     int n,k,d;
     cin>>n>>k>>d;
-    vector<vector<long long >> dp(n+1,vector<long long>(2,-1));
-    int usedBig=0;
-    find(n,k,d,usedBig,dp);
-    cout<<dp[n][usedBig];
+    vector<vector<long long >> dp(n+1,vector<long long>(2,0));
+    dp[0][1]=0;
+    dp[0][0]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+            if(i-j<0) break;
+            if(j<d){
+                dp[i][0]=(dp[i][0]+dp[i-j][0])%MOD;
+                dp[i][1]=(dp[i][1]+dp[i-j][1])%MOD;
+            }
+            else  dp[i][1]=(dp[i][1]+dp[i-j][0]+dp[i-j][1])%MOD;
+        }
+    }
+    cout<<dp[n][1]<<endl;
 }
  
 int main() {
