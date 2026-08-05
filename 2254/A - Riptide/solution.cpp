b57b186465a4da413ae0504305a7b2cb869b5c7e
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-05 21:07:36 +0530

    Add Riptide [A] from Codeforces

---
+++ 2254/A - Riptide/solution.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    vector<int> a(3);
+    for(int i=0;i<3;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+    int cnt=0;
+    // for(int x:a) cout<<x<<" ";
+    // cout<<endl;
+    while(!(a[0]==a[1] || a[1]==a[2] || a[0]==a[2])){
+       
+        a[2]-=1;
+        a[0]+=1;
+     
+        sort(a.begin(),a.end());
+        cnt++;
+    }
+    cout<<cnt<<endl;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
