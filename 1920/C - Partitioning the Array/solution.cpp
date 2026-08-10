0b0b7c4115ba20e6f5da6baadd85b849e333cabf
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-10 22:41:16 +0530

    Add Partitioning the Array [C] from Codeforces

---
+++ 1920/C - Partitioning the Array/solution.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    long long cnt=0;
+    for(int k=1;k<=n;k++){
+        if(n%k!=0) continue;
+        int gcdd=0;
+        if(k==n) {
+            cnt++;
+            break;
+        }
+        for(int i=0;i+k<n;i++){
+            gcdd=gcd(gcdd,abs(a[i]-a[i+k]));
+        }
+        if(gcdd!=1) cnt++;
+    }
+    cout<<cnt<<endl;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
