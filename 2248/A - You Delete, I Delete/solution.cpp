615f58aad63b4d7589ffc092d5871694514af91c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-12 14:56:04 +0530

    Add You Delete, I Delete [A] from Codeforces

---
+++ 2248/A - You Delete, I Delete/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    string s;
+    cin>>s;
+    char ch;
+    if(s[0]=='0') ch='1';
+    else ch='0';
+    s.erase(0,1);
+    for(int i=0;i<s.size();i++){
+        if(s[i]==ch){
+            s.erase(i,1);
+            break;
+        }
+    }
+    cout<<s<<endl;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
