40b649fbc02e107670840524918ec30031959c31
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-23 10:49:43 +0530

    Add Tape [B] from Codeforces

---
+++ 1110/B - Tape/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define ll long long
+#define endl '
+'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll modpow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+ll modinv(ll a) {
+    return modpow(a, MOD - 2);
+}
+ 
+void solve() {
+    int n,total,k;
+    cin>>n>>total>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    vector<int> diff;
+    for(int i=0;i<n-1;i++){
+        diff.push_back(a[i+1]-a[i]-1);
+    }
+   
+    sort(diff.begin(),diff.end());
+    ll ans=n;
+    for(int i=0;i<n-k;i++) ans+=diff[i];
+    cout<<ans<<endl;
+}
+ 
+int main() {
+    fastio();
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
